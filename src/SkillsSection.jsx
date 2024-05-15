5fb6b1ec930e98e023cd13274b06e01ffd5adb1e
DAjayi <dr716500@dal.ca>
2024-05-14 23:19:24 -0300

    implemented a more efficent way to manage skills listed on the page using a json file

--- src/SkillsSection.jsx
+++ src/SkillsSection.jsx
@@ -1,4 +1,7 @@
 import './assets/css/style.css'
+import data from './assets/site_data/skills_list.json'
+
+
 
 function SkillsSection(){
     return(
@@ -7,59 +10,24 @@ function SkillsSection(){
             <div className="skill_title" >My Skills</div>
 
             <div className="skill_list">
-                <div className="hidden">
-                    <a href="#"><img src="assets/images/java_logo.svg" alt="Java"/></a>
-                </div>
-                
-                <div className="hidden">
-                    <a href="#"><img src="assets/images/git_logo.svg" alt="Git"/></a>
-                </div>
-                
-                <div className="hidden">
-                    <a href="#"><img src="assets/images/python_logo.svg" alt="Python"/></a>
-                </div>
-
-                <div className="hidden">
-                    <a href="#"><img src="assets/images/csharp_logo.svg" alt="C#"/></a>
-                </div>
-
-                <div className="hidden">
-                    <a href="#"><img src="assets/images/unity_logo.svg" alt="Unity"/></a>
-                </div>
-
-                <div className="hidden">
-                    <a href="#"><img src="assets/images/css_logo.svg" alt="CSS"/></a>
-                </div>
-
-                <div className="hidden">
-                    <a href="#"><img src="assets/images/html_logo.svg" alt="HTML"/></a>
-                </div>
-
-                <div className="hidden">
-                    <a href="#"><img src="assets/images/c++_logo.svg" alt="C++"/></a>
-                </div>
-
-                <div className="hidden">
-                    <a href="#"><img src="assets/images/taliwind_logo.svg" alt="Tailwind CSS/"/></a>
-                </div>
-
-                <div className="hidden">
-                    <a href="#"><img src="assets/images/react_logo.svg" alt="React"/></a>
-                </div>
-
-                <div className="hidden">
-                    <a href="#"><img src="assets/images/mysql_logo.svg" alt="MySQL"/></a>
-                </div>
-
-                <div className="hidden">
-                    <a href="#"><img src="assets/images/javascript_logo.svg" alt="JavaScript"/></a>
-                </div>
+            
+                {Object.entries(data).map(mapValues)}
 
             </div>
             
         </div>
     )
 }
+
+function mapValues(items){
+    return(
+        <div className="hidden" key={items[0]}>
+            <a href="#"><img src={items[1]} alt={items[0]}/></a>
+        </div>
+    )
+}
+
+
 
 
 export default SkillsSection
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './assets/css/style.css'
+import '../public/js/fadeInAnimation.js'
 
 const rootElement = document.getElementById('root');
 rootElement.classList.add('root');
+window.addEventListener('load', onPageLoad);
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
 
+function onPageLoad(){
+  
 
+  let elements = document.querySelectorAll('.hidden');
+  addDelay(elements)
+  const observer = new IntersectionObserver(animateOnScroll);
+  elements.forEach((element) => observer.observe(element));
+
+}
+
+
+
+function animateOnScroll(entries) {
+  entries.forEach(entry => {
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+          // Trigger your animation logic here
+      } else {
+          entry.target.classList.remove('show');
+      }
+  });
+}
